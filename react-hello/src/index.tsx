90393fb6acb9e5d5ca3a08a84fec7842d104cd40
Jim Hessin <jhessin@gmail.com>
2021-06-27 17:18:41 -0600

    General Updates
    - added some different components and learned
    about interpolation.

--- react-hello/src/index.tsx
+++ react-hello/src/index.tsx
@@ -3,11 +3,28 @@ import ReactDOM from "react-dom";
 
 // Functional components
 function HelloWorld() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
 }
 
-//function Hello() {
-//return React.createElement("div", {}, "Hello World");
+function Hello() {
+  //const name = "Jane";
+  //return <span>Hello {name}</span>;
+  let isHello = false;
+  //return <span>{isHello ? "Hello" : "Goodbye"}</span>;
+  return (
+    <span>
+      {isHello && "Hello"}
+      {!isHello && "Goodbye"}
+    </span>
+  );
+}
+
+//function World() {
+//return <span>World</span>;
 //}
 
 ReactDOM.render(<HelloWorld />, document.querySelector("#root"));
